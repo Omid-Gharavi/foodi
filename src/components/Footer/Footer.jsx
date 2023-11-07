36203a6340f2753712dfd36f262e266b0beb8f85
Omid <omidbros3@gmail.com>
2023-11-07 13:52:25 +0330

    Footer Component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,89 @@
+import Image from 'next/image'
 import React from 'react'
+import logo from '@/assets/images/Logo.svg'
+import facebook from '@/assets/images/SocialMedia/facebook.svg'
+import instagram from '@/assets/images/SocialMedia/instagram.svg'
+import twitter from '@/assets/images/SocialMedia/twitter.svg'
+import youtube from '@/assets/images/SocialMedia/youtube.svg'
 
 const Footer = () => {
+    const icons = [
+        {
+            image: facebook,
+            invert: 'invert'
+        },
+        {
+            image: instagram,
+        },
+        {
+            image: twitter,
+        },
+        {
+            image: youtube,
+        },
+    ]
+
     return (
-        <div className='text-9xl '>Footer</div>
+        <div className='mt-60 mb-8'>
+            <div className='flex justify-center flex-wrap gap-[184px]'>
+                <div>
+                    <Image src={logo} />
+                    <p className='w-[170px] mt-[37px] text-[#555555]'>
+                        Savor the artistry where every dish is a culinary masterpiece
+                    </p>
+                </div>
+                <div className='mt-[12px] flex flex-wrap items-center justify-center gap-48'>
+
+                    <div>
+                        <p className='font-semibold'>Useful links</p>
+                        <ul className='mt-[36px] flex flex-col gap-[31px]'>
+                            <li className='text-[#555555]'>About us</li>
+                            <li className='text-[#555555]'>Events</li>
+                            <li className='text-[#555555]'>Blogs</li>
+                            <li className='text-[#555555]'>FAQ</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <p className='font-semibold'>Main Menu</p>
+                        <ul className='mt-[36px] flex flex-col gap-[31px]'>
+                            <li className='text-[#555555]'>Home</li>
+                            <li className='text-[#555555]'>Offers</li>
+                            <li className='text-[#555555]'>Menus</li>
+                            <li className='text-[#555555]'>Reservation</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <p className='font-semibold'>Contact Us</p>
+                        <ul className='mt-[36px] flex flex-col gap-[31px]'>
+                            <li className='text-[#555555]'>example@email.com</li>
+                            <li className='text-[#555555]'>+64 958 248 966</li>
+                            <li className='text-[#555555]'>Social media</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            <div className='flex items-center flex-wrap gap-[170px] mt-[100px] mx-[155px] max-md:justify-center max-lg:mx-[85px]'>
+                <div className='flex flex-wrap gap-5 max-md:justify-center'>
+                    {
+                        icons.map(icon => (
+                            <Icon image={icon.image} invert={icon.invert} />
+                        ))
+                    }
+                </div>
+                <p>Copyright 2023 Dscode | All rights reserved</p>
+            </div>
+        </div>
+    )
+}
+
+const Icon = ({ image, invert }) => {
+    return (
+        <div className='cursor-pointer bg-[#EDFFEF] hover:bg-[#39DB4A] transition duration-[0.2s] ease-in-out w-14 h-14 rounded-full flex justify-center items-center'>
+            <Image className={`${invert ?? ''}`} src={image} alt='media' />
+        </div>
     )
 }
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,6 +18,7 @@ export default function App({ Component, pageProps }) {
             <>
               <Header />
               <Component {...pageProps} />
+              <Footer />
             </>
           )
       }
